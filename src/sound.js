6b4f81a6e1bf781cd1e2cb02d66c26c2a624ce59
Alpy81 <alperen.ertas@gmx.de>
2024-01-29 22:56:05 +0100

    update commit

--- src/sound.js
+++ src/sound.js
@@ -1,11 +1,11 @@
 export const SOUND = {
-  CUE: new Audio("../dist/sfx/cue.mp3"),
-  COLLISION: new Audio("../dist/sfx/collision.mp3"),
-  BUMPER: new Audio("../dist/sfx/bumper.mp3"),
-  POCKET: new Audio("../dist/sfx/pocket.mp3"),
-  WHIP: new Audio("../dist/sfx/whip.mp3"),
-  WIN: new Audio("../dist/sfx/win.mp3"),
-  LOSE: new Audio("../dist/sfx/lose.mp3"),
+  CUE: new Audio("../sfx/cue.mp3"),
+  COLLISION: new Audio("../sfx/collision.mp3"),
+  BUMPER: new Audio("../sfx/bumper.mp3"),
+  POCKET: new Audio("../sfx/pocket.mp3"),
+  WHIP: new Audio("../sfx/whip.mp3"),
+  WIN: new Audio("../sfx/win.mp3"),
+  LOSE: new Audio("../sfx/lose.mp3"),
 };
 
 SOUND.WHIP.volume = 0.2;
